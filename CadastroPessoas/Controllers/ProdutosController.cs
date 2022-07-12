2961123567f65c6663078cd7f75bd878de751cf0
Carlos-Simim <carlinhossimim@hotmail.com>
2022-07-12 17:14:18 -0300

    Tentando arrumar a criação de novos produtos

--- CadastroPessoas/Controllers/ProdutosController.cs
+++ CadastroPessoas/Controllers/ProdutosController.cs
@@ -1,6 +1,7 @@
 ﻿using CadastroProdutos.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WebMatrix.Data;
 
 namespace CadastroProdutos.Controllers
 {
@@ -13,7 +14,7 @@ namespace CadastroProdutos.Controllers
             _contexto = contexto;
         }
         
-        public ActionResult Index()
+        public IActionResult Index()
         {            
             var produtos = _contexto.Produtos.Include(p => p.DefSituacaoProduto);
             return View( produtos.ToList());
@@ -26,13 +27,46 @@ namespace CadastroProdutos.Controllers
         }
 
         [HttpPost]
-        public ActionResult CriarProduto(Produto produto)
+        public IActionResult CriarProduto(string Descricao, int DefSituacaoProduto, int DefUnidadeComercial, decimal PesoLiquido)
         {
-            
-            _contexto.Add(produto);
+
+            /*_contexto.Add(produto);
             _contexto.SaveChanges();
-            return RedirectToAction(nameof(Index));
+
+            return RedirectToAction(nameof(Index));*/
+
+            var novoProduto = new Produto()
+            {
+                Descricao = Descricao,
+                SituacaoProdutoId = DefSituacaoProduto,
+                DefUnidadeComercial = DefUnidadeComercial,
+                PesoLiquido = PesoLiquido,
+            };
+
+
+
+            /*novoProduto.Descricao = Descricao;
+            novoProduto.DefSituacaoProduto = DefSituacaoProduto;
+            novoProduto.DefUnidadeComercial = DefUnidadeComercial;
+            novoProduto.PesoLiquido = PesoLiquido;*/
             
+
+
+            _contexto.Add(novoProduto);
+            _contexto.SaveChanges();
+            
+            /*var db = Database.Open("CadastroProdutos");
+            var insertCommand = "INSERT INTO Produtos VALUES (@0, @1, @2, @3)";
+            db.Execute(insertCommand, Descricao, DefSituacaoProduto, DefUnidadeComercial, PesoLiquido);*/
+            //return RedirectToAction(nameof(Index));
+
+            /*
+             var db = Database.Open("WebPagesMovies");
+            var insertCommand = "INSERT INTO Movies (Title, Genre, Year) VALUES(@0, @1, @2)";
+            db.Execute(insertCommand, title, genre, year);
+            */
+
+            return Content($"Descrição: {Descricao}\nSituação: {DefSituacaoProduto}\nUnidade: {DefUnidadeComercial}\nPeso: {PesoLiquido}");
         }
 
         [HttpGet]
