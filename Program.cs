c4e5774359b39338abb909a0e8e578a829f494d7
Carlos-Simim <42616810+Carlos-Simim@users.noreply.github.com>
2022-07-12 12:12:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using CadastroProdutos.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Contexto>(opcoes => opcoes.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CadastroProdutos;"));
+
+//ConfigurationManager.ConnectionStrings["ConexaoBd"].ConnectionString;
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+//Define a página inicial
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
