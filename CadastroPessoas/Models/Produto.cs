2577fde6e4a061cb0ab96a8889e2ace688cfa29a
Carlos-Simim <carlinhossimim@hotmail.com>
2022-07-12 13:46:11 -0300

    Ultima versão funcionando normalmente

--- CadastroPessoas/Models/Produto.cs
+++ CadastroPessoas/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CadastroProdutos.Models
@@ -6,20 +7,25 @@ namespace CadastroProdutos.Models
     public class Produto
     {
         [Key]
+        //[DisplayName("Código")]
         public int ProdutoID { get; set; }
 
         /*[ForeignKey("ProdutoEmbalagemID")]
         public virtual ProdutoEmbalagem ProdutoEmbalagem { get; set; }*/
 
         [Column(TypeName = "varchar(120)")]
+        //[DisplayName("Descrição")]
         public string Descricao { get; set; }
 
         [ForeignKey("SituacaoProdutoID")]
+       //[DisplayName("Situação")]
         public virtual DefSituacaoProduto DefSituacaoProduto { get; set;}
 
         [ForeignKey("UnidadeComercialID")]
+        //[DisplayName("Unidade comercial")]
         public virtual DefUnidadeComercial DefUnidadeComercial { get; set;}
 
+        //[DisplayName("Peso Líquido")]
         public decimal PesoLiquido { get; set; }
 
     }
